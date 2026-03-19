fda2d3a3f48a9c1469e309a925658378b1d774ad
davidvaldez050708@gmail.com <davidvaldez050708@gmail.com>
2026-03-19 15:26:09 -0600

    Modelos

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'edad',
+        'cargo',
+        'turno',
+        'role',
         'password',
     ];
 
